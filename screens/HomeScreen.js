9a3daf8b6c8a7d6eea59cc0f0ad7572efecb4190
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2023-04-16 20:00:50 +0100

    Added Search Box

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,12 @@
-import { View, Text, Image, Alert } from 'react-native';
+import { View, Text, Image, Alert, Pressable, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import * as Location from 'expo-location';
 import { MaterialIcons } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
+import { SliderBox } from "react-native-image-slider-box";
+
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -70,14 +73,38 @@ const HomeScreen = () => {
     }, []);
   return ( 
     <SafeAreaView>
-       <View>
+      {/*Location and Profile*/}
+       <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
           <MaterialIcons name="location-on" size={30} color="#EF0107" />
           <View>
             <Text style={{fontSize: 18, fontWeight: "800"}}>Home</Text>
-          </View>
-
-          <Text>{displayCurrentAddress}</Text> 
+            <Text>{displayCurrentAddress}</Text>
+          </View> 
+          <Pressable style={{ marginLeft: "auto",marginRight: 7 }}>
+            <Image 
+            style={{ width: 40, height: 40, borderRadius: 28 }} 
+            source={{
+              uri:"https://lh3.googleusercontent.com/ogw/AOLn63E2k-hU-mfELjIZ_d2iImNJNmD1twrs-RfH4yNMLg=s32-c-mo"
+            }}
+            />
+          </Pressable>
        </View>
+       {/*Search Bar*/}
+       <View 
+       style={{
+          padding: 10, 
+          margin: 10, 
+          flexDirection: "row", 
+          alignitems: "center ",
+          justifyContent: "space-between",
+          borderWidth: 8.8,
+          borderColor: "#c0c0c0",
+          borderRadius: 2
+          }}
+        >
+        <TextInput placeholder="Search for items or More"/>
+        <EvilIcons name="search" size={24} color="#EF0107" />
+        </View> 
     </SafeAreaView>
   )
 }
